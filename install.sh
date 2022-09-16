f91dfcd5bf1c48cc2b3f1e5a63f086544b5562c5
Raffaello Bonghi <raffaello@rnext.it>
2022-09-16 11:05:42 +0100

    Fix new docker compose install

--- install.sh
+++ install.sh
@@ -54,6 +54,13 @@ install_jetson()
     # https://docs.docker.com/engine/install/ubuntu/#set-up-the-repository
         sudo apt-get update
         sudo apt-get install -y ca-certificates curl gnupg lsb-release
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
     fi
 
     local PATH_HOST_FILES4CONTAINER="/etc/nvidia-container-runtime/host-files-for-container.d"
